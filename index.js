c5067e7572b10bf83e86870c9570b47d0702aedf
VladeJunior <137967543+VladeJunior@users.noreply.github.com>
2026-04-23 17:40:18 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -190,7 +190,7 @@ function signNFe(xmlNFe, keyPem, certPem) {
     privateKey: keyPem,
     publicCert: certPem,
     signatureAlgorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1",
-    canonicalizationAlgorithm: "http://www.w3.org/2001/10/xml-exc-c14n#",
+    canonicalizationAlgorithm: "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
     getKeyInfoContent: () => `<X509Data><X509Certificate>${certBody}</X509Certificate></X509Data>`,
   });
 
@@ -199,7 +199,7 @@ function signNFe(xmlNFe, keyPem, certPem) {
     digestAlgorithm: "http://www.w3.org/2000/09/xmldsig#sha1",
     transforms: [
       "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
-      "http://www.w3.org/2001/10/xml-exc-c14n#",
+      "http://www.w3.org/TR/2001/REC-xml-c14n-20010315",
     ],
   });
 
